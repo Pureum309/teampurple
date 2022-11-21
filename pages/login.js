c689bf1d60acfa255d5524023cbfd258a31e0113
Pureum309 <psun27@my.bcit.ca>
2022-11-21 13:58:39 -0800

    tyring to route

--- pages/login.js
+++ pages/login.js
@@ -1,59 +1,59 @@
-import React, { useRef, useState } from "react";
+// import React, { useRef, useState } from "react";
 
-import { signup, login, logout, useAuth } from "../firebase/firebase.config.js";
+// import { signup, login, logout, useAuth } from "../firebase/firebase.config.js";
 
-export default function App() {
-  const [ loading, setLoading ] = useState(false);
-  const currentUser = useAuth();
+// export default function App() {
+//   const [ loading, setLoading ] = useState(false);
+//   const currentUser = useAuth();
 
-  const emailRef = useRef();
-  const passwordRef = useRef();
+//   const emailRef = useRef();
+//   const passwordRef = useRef();
 
-  async function handleSignup() {
-    setLoading(true);
-    // try {
-      await signup(emailRef.current.value, passwordRef.current.value);
-    // } catch {
-      // alert("Error!");
-    // }
-    setLoading(false);
-  }
+//   async function handleSignup() {
+//     setLoading(true);
+//     // try {
+//       await signup(emailRef.current.value, passwordRef.current.value);
+//     // } catch {
+//       // alert("Error!");
+//     // }
+//     setLoading(false);
+//   }
 
-  async function handleLogin() {
-    setLoading(true);
-    try {
-      await login(emailRef.current.value, passwordRef.current.value);
+//   async function handleLogin() {
+//     setLoading(true);
+//     try {
+//       await login(emailRef.current.value, passwordRef.current.value);
       
-    } catch {
-      alert("Error!");
-    }
-    setLoading(false);
-  }
+//     } catch {
+//       alert("Error!");
+//     }
+//     setLoading(false);
+//   }
 
-  async function handleLogout() {
-    setLoading(true);
-    try {
-      await logout();
-    } catch {
-      alert("Error!");
-    }
-    setLoading(false);
-  }
+//   async function handleLogout() {
+//     setLoading(true);
+//     try {
+//       await logout();
+//     } catch {
+//       alert("Error!");
+//     }
+//     setLoading(false);
+//   }
 
-  return (
-    <div id="main">
+//   return (
+//     <div id="main">
       
-      <div>Currently logged in as: { currentUser?.email } </div>
+//       <div>Currently logged in as: { currentUser?.email } </div>
 
-      <div id="fields">
-        <input ref={emailRef} placeholder="Email" />
-        <input ref={passwordRef} type="password" placeholder="Password" />
-      </div>
+//       <div id="fields">
+//         <input ref={emailRef} placeholder="Email" />
+//         <input ref={passwordRef} type="password" placeholder="Password" />
+//       </div>
 
-      <button disabled={ loading || currentUser } onClick={handleSignup}>Sign Up</button>
-      <button disabled={ loading || currentUser } onClick={handleLogin}>Log In</button>
-      <button disabled={ loading || !currentUser } onClick={handleLogout}>Log Out</button>
+//       <button disabled={ loading || currentUser } onClick={handleSignup}>Sign Up</button>
+//       <button disabled={ loading || currentUser } onClick={handleLogin}>Log In</button>
+//       <button disabled={ loading || !currentUser } onClick={handleLogout}>Log Out</button>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
