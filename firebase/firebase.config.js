41565bc954480cb22167b96460e2afff8eb58826
kaleytsangbcit <ktsang38@my.bcit.ca>
2022-11-20 21:56:41 -0800

    Create Post done

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCIR5GSNhNUqJ-KZMVAyz3L0inw_ATm16I",
@@ -13,7 +14,7 @@ const firebaseConfig = {
   };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
 export function signup(email, password) {
   return createUserWithEmailAndPassword(auth, email, password);
@@ -38,3 +39,5 @@ export function useAuth() {
 
   return currentUser;
 }
+
+export const db = getFirestore(app);
