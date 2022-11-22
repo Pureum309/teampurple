2d219a9601d8a6915a632bdf358b5658ffc7788e
kaleytsangbcit <ktsang38@my.bcit.ca>
2022-11-22 11:35:12 -0800

    adding gitignore file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0adbe8fb0ed327b7"; }
+/******/ 		__webpack_require__.h = function() { return "775e88cfc3c1b438"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
